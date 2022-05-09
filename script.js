4b7932bb9ee58cf6ab667e283cccfa7e053dfe53
monsalex23 <monsalex23@gmail.com>
2022-05-08 21:05:14 -0300

    add estilos y logica

--- script.js
+++ script.js
@@ -1,34 +1,72 @@
 const dino = document.querySelector('.dino');
+const background = document.querySelector('.background');
+
 let isJumping = false;
+let isGameOver = false;
+let position = 0;
 
 function handleKeyUp(event) {
     if (event.keyCode === 32) {
-        jump();
+        if (!isJumping) {
+            jump();
+        }
     }
 }
+
 function jump() {
-    let position = 0;
+    isJumping = true;
 
     let upInterval = setInterval(() => {
         if (position >= 150) {
-            // Descendo
+            // bajando
             clearInterval(upInterval);
 
             let downInterval = setInterval(() => {
                 if (position <= 0) {
                     clearInterval(downInterval);
-
+                    isJumping = false;
                 } else {
                     position -= 20;
                     dino.style.bottom = position + 'px';
                 }
             }, 20);
         } else {
-            // Subindo
+            // Subiendo
             position += 20;
             dino.style.bottom = position + 'px';
         }
     }, 20);
 }
-document.addEventListener('keyup', handleKeyUp);
 
+function createCactus() {
+    const cactus = document.createElement('div');
+    let cactusPosition = 1000;
+    let randomTime = Math.random() * 6000;
+
+    if (isGameOver) return;
+
+    cactus.classList.add('cactus');
+    background.appendChild(cactus);
+    cactus.style.left = cactusPosition + 'px';
+
+    let leftTimer = setInterval(() => {
+        if (cactusPosition < -60) {
+            // se fue de a tela
+            clearInterval(leftTimer);
+            background.removeChild(cactus);
+        } else if (cactusPosition > 0 && cactusPosition < 60 && position < 60) {
+            // Game over
+            clearInterval(leftTimer);
+            isGameOver = true;
+            document.body.innerHTML = '<h1 class="game-over">Fim de jogo</h1>';
+        } else {
+            cactusPosition -= 10;
+            cactus.style.left = cactusPosition + 'px';
+        }
+    }, 20);
+
+    setTimeout(createCactus, randomTime);
+}
+
+createCactus();
+document.addEventListener('keyup', handleKeyUp);
